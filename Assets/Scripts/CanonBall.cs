dc1ee2029e83dae19f9ac713aa875a0341d7c54b
Achmad Royhan Kamil <royhan1125@gmail.com>
2024-08-06 14:15:08 +0700

    Update CanonBall.cs

--- Assets/Scripts/CanonBall.cs
+++ Assets/Scripts/CanonBall.cs
@@ -14,4 +14,12 @@ public class CanonBall : MonoBehaviour
         _rigidbody.AddForce(fireforce, ForceMode.Impulse);
         _rigidbody.angularVelocity = new Vector3 (Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
     }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        _rigidbody.angularVelocity = Vector3.zero;
+        _rigidbody.velocity = Vector3.zero;
+        _rigidbody.isKinematic = true;
+        _rigidbody.detectCollisions = false;
+    }
 }
