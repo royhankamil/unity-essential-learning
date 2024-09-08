98c9fd9b9d2cb074e4e1579effa50f411641dced
Achmad Royhan Kamil <royhan1125@gmail.com>
2024-09-08 16:44:44 +0700

    animation

--- Assets/Scripts/CanonBall.cs
+++ Assets/Scripts/CanonBall.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class CanonBall : MonoBehaviour
 {
+    private static readonly int ExplodeHash = Animator.StringToHash("Exploded");
     private Rigidbody _rigidbody;
+    public Animator animator;
 
     void Awake()
     {
@@ -15,11 +17,18 @@ public class CanonBall : MonoBehaviour
         _rigidbody.angularVelocity = new Vector3 (Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
     }
 
+    public void OnFinishedExplosionAnimation()
+    {
+        Destroy(gameObject);
+    }
+
     private void OnCollisionEnter(Collision other) 
     {
         _rigidbody.angularVelocity = Vector3.zero;
         _rigidbody.velocity = Vector3.zero;
         _rigidbody.isKinematic = true;
         _rigidbody.detectCollisions = false;
+
+        animator.SetTrigger(ExplodeHash);
     }
 }
